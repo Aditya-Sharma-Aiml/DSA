ce7bd828f87bff0ef2f0895bb83b7cd4fe4fcf27
Aditya Sharma <adityasharma.cseaiml@gmail.com>
2025-08-10 00:24:14 +0530

    sorting 	new file:   Basics/sorting/bubbleSortRecursive.cpp 	modified:   Basics/sorting/insertionSort.cpp 	new file:   Basics/sorting/insertionSortRecursive.cpp

--- Basics/sorting/insertionSort.cpp
+++ Basics/sorting/insertionSort.cpp
@@ -5,8 +5,8 @@ void insertionSort(int arr[],int n){
     
     for(int i=0 ; i<n ;i++){
          int j=i;
-        while(j>0 && arr[j]<arr[j-1]){
-            swap(arr[j],arr[j-1]);
+        while(j>0 && arr[j - 1] > arr[j]){
+            swap(arr[j-1],arr[j]);
             j--;
         }
     }
