dd16e0098175fdac0f054130d391b6eee6984b7c
Aditya Sharma <adityasharma.cseaiml@gmail.com>
2025-10-31 17:50:53 +0530

    BinarySearch 	new file:   BinarySearch/BS_on_Answers/paintersPartition.cpp 	new file:   BinarySearch/BS_on_Answers/splitArraySubArraySum.cpp

---
+++ BinarySearch/BS_on_Answers/paintersPartition.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//  xxxxxx Same As BOOK ALLOCATION xxxxxx
+
+// Time Complexity: O(N * log(sum(arr[])-max(arr[])+1))
+
+int countPainters(vector<int> &boards, int time) {
+
+    int n = boards.size();
+
+    int painters = 1;
+    long long boardsPainter = 0;
+
+    for (int i = 0; i < n; i++) {
+
+        if (boardsPainter + boards[i] <= time) {
+            //allocate board to current painter
+            boardsPainter += boards[i];
+        }
+        else {
+            //allocate board to next painter
+            painters++;
+            boardsPainter = boards[i];
+        }
+    }
+    return painters;
+}
+
+int findLargestMinDistance(vector<int> &boards, int k) {
+
+    int low = *max_element(boards.begin(), boards.end());
+    int high = accumulate(boards.begin(), boards.end(), 0);
+    //Apply binary search:
+    while (low <= high) {
+
+        int mid = (low + high) / 2;
+
+        int painters = countPainters(boards, mid);
+
+        if (painters > k) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return low;
+}
+int main()
+{
+    vector<int> boards = {10, 20, 30, 40};
+    int k = 2;
+    int ans = findLargestMinDistance(boards, k);
+    cout << "The answer is: " << ans << "\n";
+    return 0;
+}
